c31e1d4868f666e7c231a474df9a48500d6ae315
chiragsenma910 <139469416+chiragsenma910@users.noreply.github.com>
2025-01-12 04:38:12 +0000

    done

---
+++ 01_Basic/01_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+
+//alert(4+5) // we are using nodejs,  not brower
+
+
+// let name="Akash"
+// let age=55
+// let isLoggedIn = false
+
+
+//number=> 2 to power 53
+// bigint 
+// string =>""
+//boolean=true/false
+//null  => standalone value
+// undefied =>
+// symbol =>unique
+
+// object
+
+console.log(typeof "Chirag");
---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,22 @@
+const accountId=12445
+let accountEmail="Chirag@hmail.com"
+var accountPassword="5442522" 
+accountCity="Mehsana"
+
+let accountState;
+
+//accountId=12225 Not Allowed
+// console.log(accountId);
+
+accountEmail="Chirag0910@gmail.com"
+accountPassword="85495"
+accountCity="Baroda"
+
+
+/*
+Preferd not to use var
+beacuse of the issue of the block scope and the function scope 
+*/
+  
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountState])
