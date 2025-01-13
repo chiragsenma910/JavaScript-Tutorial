6c052a226b03aa136939284f80da07172dc7828d
chiragsenma910 <139469416+chiragsenma910@users.noreply.github.com>
2025-01-13 04:13:37 +0000

    Operation and Comparison

---
+++ 01_Basic/03_conversionOperation.js
@@ -0,0 +1,61 @@
+// let score ="chirag"
+
+// console.log(typeof score) ;
+// console.log(typeof(score));
+
+
+
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+// after the conversion
+// "33" => 33
+// "33abc" =>NaN  (not a number)
+// true=> 1 =>; false =>0
+
+
+// let isLoggedIn=1
+
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true ; 0=> false
+// ""(empty string)
+// "hello" => true
+
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+
+
+// *************************Operation **************************
+
+let value =3
+let negValue=-value
+// console.log(negValue);
+ 
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**5);
+// console.log(2%2);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
