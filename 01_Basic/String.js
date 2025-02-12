6d3301a6d2062c56561287347eadf738d53c3e5d
chiragsenma910 <139469416+chiragsenma910@users.noreply.github.com>
2025-02-12 10:07:43 +0530

    Add files via upload

---
+++ 01_Basic/String.js
@@ -0,0 +1,23 @@
+const name='Chirag';
+
+const repoCount=50
+
+console.log(`Hello My name is ${name} and My RepoCount is ${repoCount}`);
+
+const gameName= new String('Chirag');
+
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(3)); //find index to which character exist
+console.log(gameName.indexOf('r')); // find index through a character
+
+console.log(gameName.substring(0,4));// we don't add negative value
+
+console.log(gameName.slice(-6,2));
+
+const newName='   chirag  '
+console.log(newName);
+console.log(newName.trim()); // remove whitespace 
+
+
