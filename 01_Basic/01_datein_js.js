e4735a12cfc7c3cf1e61c83a7b5cdd9d3f433d59
chiragsenma910 <139469416+chiragsenma910@users.noreply.github.com>
2025-02-18 09:55:55 +0530

    Add files via upload

---
+++ 01_Basic/01_datein_js.js
@@ -0,0 +1,34 @@
+//Dates
+
+let myDate =new Date()
+
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+
+console.log(typeof myDate);
+
+
+// let myCreatedDate=new Date(2025,1,20);
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedDate=new Date("2025-01-13");
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp=Date.now()
+
+console.log(myTimeStamp);
+
+let newDate=new Date()
+console.log(newDate);
+
+console.log(newDate.getMonth()+ 1);
+console.log(newDate.getDay());
+
+
+
+
+
+
+
+
